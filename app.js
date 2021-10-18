0ab25b2ac85673c242e8147bf4a4833f93c73298
Daniel <coding.dotping@gmail.com>
2021-10-18 18:01:24 +1100

    set up the db connection with mongodb, mongoose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connection');
 
 //middleware
 app.use(express.json());
@@ -12,13 +14,16 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks');
-// app.post('/api/v1/tasks');
-// app.get('/api/v1/tasks/:id');
-// app.patch('/api/v1/tasks/:id');
-// app.delete('/api/v1/tasks/:id');
-
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.error(error)
+    }
+}
 
+
+start();
